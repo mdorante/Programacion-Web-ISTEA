e0503c7710710ee5cde3354f53a0a2082da67828
mdorante <mdorante10@gmail.com>
2021-09-19 19:32:01 -0300

    add new properties and functions for TablaAlumnosComponent

--- Clase3-DirectivasYForms/directivasapp/src/app/components/tabla-alumnos/tabla-alumnos.component.ts
+++ Clase3-DirectivasYForms/directivasapp/src/app/components/tabla-alumnos/tabla-alumnos.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Alumno } from '../entities/alumno';
 
 const ALUMNOS = [
-  new Alumno('Jose', 'Martinez', 'Python', '../../assets/images/foto.jpg'),
-  new Alumno('Martina', 'Perez', 'Angular', '../../assets/images/foto.jpg'),
-  new Alumno('Robert', 'Fernandez', 'Go', '../../assets/images/foto.jpg'),
+  new Alumno('Jose', 'Martinez', 'Python', '../../assets/images/foto1.jpeg'),
+  new Alumno('Martina', 'Perez', 'Angular', '../../assets/images/foto2.jpeg'),
+  new Alumno('Robert', 'Fernandez', 'Go', '../../assets/images/foto3.jpeg'),
 ];
 
 @Component({
@@ -17,7 +17,24 @@ export class TablaAlumnosComponent implements OnInit {
   saludar: boolean = true;
   alumnoSeleccionado: Alumno;
 
+  btnOk = 'btn- btn-success';
+  btnNo = 'btn btn-danger';
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  establecerSaludo() {
+    if (this.saludar) {
+      this.saludar = false;
+    } else {
+      this.saludar = true;
+    }
+  }
+
+  varAlumno(alumno: Alumno) {
+    console.log('Seleccionaste el siguiente alumno al hacer click');
+    console.dir(alumno);
+    this.alumnoSeleccionado = alumno;
+  }
 }

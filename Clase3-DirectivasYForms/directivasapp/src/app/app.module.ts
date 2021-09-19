fb004fbe2b42601da5172a3899ca2eb032e5458e
mdorante <mdorante10@gmail.com>
2021-09-19 18:22:01 -0300

    add tabla-alumnos component

--- Clase3-DirectivasYForms/directivasapp/src/app/app.module.ts
+++ Clase3-DirectivasYForms/directivasapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TablaAlumnosComponent } from './components/tabla-alumnos/tabla-alumnos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaAlumnosComponent
   ],
   imports: [
     BrowserModule,

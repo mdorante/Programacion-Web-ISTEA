d07853625ebed28cc150ec9a00888fa48e0012dc
mdorante <mdorante10@gmail.com>
2021-09-19 20:10:40 -0300

    add properties and funcs to HomeComponent

--- Clase3-DirectivasYForms/bike-ui/src/app/components/home/home.component.ts
+++ Clase3-DirectivasYForms/bike-ui/src/app/components/home/home.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  // creamos un array con los modelos de bicicletas que se podran seleccionar
+  models: string[] = [
+    'bike mistica 2021',
+    'bike bizarra',
+    'bike covid',
+    'bike spidey',
+  ];
 
-  constructor() { }
+  // este mensaje lo utilizaremos para mostrar al user si su formulario se pudo enviar al backend o no
+  validMessage: string = '';
 
-  ngOnInit(): void {
+  bikeForm: FormGroup;
+
+  constructor() {
+    this.bikeForm = new FormGroup({});
   }
 
+  ngOnInit(): void {}
+
+  // esta funcion sera invocada cuando se intente enviar el form al backend
+  submitRegistration() {}
 }
